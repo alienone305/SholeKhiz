56be104b0fe7e2ee7337ae96e1def54b491f75a8
alienone304 <alienone304@gmail.com>
2021-10-28 20:12:41 +0330

    OCT 26

--- company/utils.py
+++ company/utils.py
@@ -3,11 +3,13 @@ from urllib.parse import quote
 from django.conf import settings
 
 def SendMessage(phone_number, text):
+    print('xxxxxxxxxxxxxxx')
     url = "https://login.niazpardaz.ir/SMSInOutBox/SendSms?username={username}&password={password}&from={line_number}&to={phone_number}&text=".format(
     phone_number = phone_number, username = settings.NIAZPARDAZ_USERNAME, password = settings.NIAZPARDAZ_PASSWORD,
     line_number = settings.NIAZPARDAZ_LINENUMBER)
     text = quote(text)
     persian_url = url + text
+    print(persian_url)
     page = urlopen(persian_url)
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
